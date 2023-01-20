7f12262fed215efd8e0cfd117e6bd4b915c43a8b
Alex J. Malozemoff <amaloz@galois.com>
2023-01-20 15:22:42 -0800

    Add nonce masking operation to `PaillierEncryptionKey`
    This commit adds a new `PaillierEncryptionKey::mask` method, which
    takes two nonces and produces a masked nonce. It also threads the
    `MaskedNonce` type through the ZKPs, although currently we still use
    `.0` to extract the underlying `BigNumber`. This will change once we
    add some additional functionality for operating on `MaskedNonce`s.

--- src/zkp/pienc.rs
+++ src/zkp/pienc.rs
@@ -14,7 +14,7 @@
 use super::Proof;
 use crate::{
     errors::*,
-    paillier::{PaillierCiphertext, PaillierEncryptionKey, PaillierNonce},
+    paillier::{MaskedNonce, PaillierCiphertext, PaillierEncryptionKey, PaillierNonce},
     parameters::{ELL, EPSILON},
     utils::{k256_order, modpow, plusminus_bn_random_from_transcript, random_bn_in_range},
     zkp::setup::ZkSetupParameters,
@@ -32,7 +32,7 @@ pub(crate) struct PiEncProof {
     C: BigNumber,
     e: BigNumber,
     z1: BigNumber,
-    z2: BigNumber,
+    z2: MaskedNonce,
     z3: BigNumber,
 }
 
@@ -92,8 +92,6 @@ impl Proof for PiEncProof {
         let mu = random_bn_in_range(rng, ELL) * &input.setup_params.N;
         let gamma = random_bn_in_range(rng, ELL + EPSILON) * &input.setup_params.N;
 
-        let N0 = input.pk.n();
-
         let S = {
             let a = modpow(&input.setup_params.s, &secret.k, &input.setup_params.N);
             let b = modpow(&input.setup_params.t, &mu, &input.setup_params.N);
@@ -122,7 +120,7 @@ impl Proof for PiEncProof {
         );
 
         let z1 = &alpha + &e * &secret.k;
-        let z2 = r.inner().modmul(&modpow(secret.rho.inner(), &e, N0), N0);
+        let z2 = input.pk.mask(&r, &secret.rho, &e);
         let z3 = gamma + &e * mu;
 
         let proof = Self {
@@ -166,7 +164,7 @@ impl Proof for PiEncProof {
 
         let eq_check_1 = {
             let a = modpow(&(&BigNumber::one() + N0), &self.z1, &N0_squared);
-            let b = modpow(&self.z2, N0, &N0_squared);
+            let b = modpow(&self.z2.0, N0, &N0_squared);
             let lhs = a.modmul(&b, &N0_squared);
             let rhs = self
                 .A
