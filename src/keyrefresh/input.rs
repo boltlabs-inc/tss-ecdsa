1b8415d0ee9dede160b09fd4820559d410483f31
naure <naure@users.noreply.github.com>
2024-04-12 20:49:59 +0200

    Keyrefresh protocol (#1)
    * Init keyrefresh module
    
    * Add MessageType and BroadcastTag for keyrefresh.
    
    * Fix test
    
    * keyrefresh: protocol description
    
    * keyrefresh: Remove rid
    
    * keyrefresh: generate key updates for all participants.
    
    * keyrefresh: key updates sum to 0. Store updates.
    
    * keyrefresh: generate, broadcast, and verify proofs. Remove keygen variant.
    
    * keyrefresh: aggregate updates
    
    * keyrefresh: send and receive private updates
    
    * keyrefresh: pass inputs from keygen and auxinfo
    
    * keyrefresh: encrypt and decrypt the private updates
    
    * keyrefresh: verify_with_precommit
    
    * keyrefresh: all checks for KeyrefreshDecommit
    
    ---------
    
    Co-authored-by: Aurélien Nicolas <aurel@inversed.tech>

---
+++ src/keyrefresh/input.rs
@@ -0,0 +1,251 @@
+// Copyright (c) 2022-2023 Bolt Labs Holdings, Inc
+//
+// This source code is licensed under both the MIT license found in the
+// LICENSE-MIT file in the root directory of this source tree and the Apache
+// License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+// of this source tree.
+use std::collections::HashSet;
+
+use tracing::error;
+
+use crate::{
+    auxinfo::{self, AuxInfoPrivate, AuxInfoPublic},
+    errors::{CallerError, InternalError, Result},
+    keygen::{self, KeySharePrivate, KeySharePublic},
+    ParticipantIdentifier,
+};
+
+/// Input needed for a
+/// [`KeyrefreshParticipant`](crate::keyrefresh::KeyrefreshParticipant) to run.
+#[derive(Debug, Clone)]
+pub struct Input {
+    /// The key share material for the key that will be refreshed.
+    keygen_output: keygen::Output,
+    /// The auxiliary info to encrypt/decrypt messages with other participants.
+    auxinfo_output: auxinfo::Output,
+}
+
+impl Input {
+    /// Creates a new [`Input`] from the outputs of the
+    /// [`auxinfo`](crate::auxinfo::AuxInfoParticipant) and
+    /// [`keygen`](crate::keygen::KeygenParticipant) protocols.
+    pub fn new(auxinfo_output: auxinfo::Output, keygen_output: keygen::Output) -> Result<Self> {
+        if auxinfo_output.public_auxinfo().len() != keygen_output.public_key_shares().len() {
+            error!(
+                "Number of auxinfo ({:?}) and keyshare ({:?}) public entries is not equal",
+                auxinfo_output.public_auxinfo().len(),
+                keygen_output.public_key_shares().len()
+            );
+            Err(CallerError::BadInput)?
+        }
+
+        // The same set of participants must have produced the key shares and aux infos.
+        let aux_pids = auxinfo_output
+            .public_auxinfo()
+            .iter()
+            .map(AuxInfoPublic::participant)
+            .collect::<HashSet<_>>();
+        let key_pids = keygen_output
+            .public_key_shares()
+            .iter()
+            .map(KeySharePublic::participant)
+            .collect::<HashSet<_>>();
+        if aux_pids != key_pids {
+            error!("Public auxinfo and keyshare inputs to presign weren't from the same set of parties.");
+            Err(CallerError::BadInput)?
+        }
+
+        // There shouldn't be duplicates.
+        // This check is redundant, since it's also checked in the `auxinfo::Output` and
+        // `keygen::Output` constructors, so we actually don't test it below.
+        // Also, since we checked equality of the sets and the lengths already, checking
+        // for keygen also validates it for auxinfo
+        if key_pids.len() != keygen_output.public_key_shares().len() {
+            error!("Duplicate participant IDs appeared in AuxInfo and KeyShare public input.");
+            Err(CallerError::BadInput)?
+        }
+
+        // The constructors for keygen and auxinfo output already check other important
+        // properties, like that the private component maps to one of public
+        // components for each one.
+
+        // The participant IDs for the private components of each output should match
+        if keygen_output.private_pid() != auxinfo_output.private_pid() {
+            error!("Expected private keygen and auxinfo outputs to correspond to the same participant, but they didn't");
+            Err(CallerError::BadInput)?
+        }
+
+        Ok(Self {
+            auxinfo_output,
+            keygen_output,
+        })
+    }
+
+    pub(crate) fn public_key_shares(&self) -> &[KeySharePublic] {
+        self.keygen_output.public_key_shares()
+    }
+
+    /// Get the set of participants that contributed to the input.
+    ///
+    /// By construction, this must be the same for the auxinfo and key share
+    /// lists.
+    pub(crate) fn participants(&self) -> Vec<ParticipantIdentifier> {
+        self.keygen_output
+            .public_key_shares()
+            .iter()
+            .map(KeySharePublic::participant)
+            .collect()
+    }
+
+    pub(crate) fn private_auxinfo(&self) -> &AuxInfoPrivate {
+        self.auxinfo_output.private_auxinfo()
+    }
+
+    /// Returns the [`AuxInfoPublic`] associated with the given
+    /// [`ParticipantIdentifier`].
+    pub(crate) fn find_auxinfo_public(&self, pid: ParticipantIdentifier) -> Result<&AuxInfoPublic> {
+        self.auxinfo_output.find_public(pid)
+            .ok_or_else(|| {
+                error!("Presign input doesn't contain a public auxinfo for {}, even though we checked for it at construction.", pid);
+                InternalError::InternalInvariantFailed
+            })
+    }
+
+    pub(crate) fn private_key_share(&self) -> &KeySharePrivate {
+        self.keygen_output.private_key_share()
+    }
+
+    /// Returns the [`KeySharePublic`] associated with the given
+    /// [`ParticipantIdentifier`].
+    pub(crate) fn find_keyshare_public(
+        &self,
+        pid: ParticipantIdentifier,
+    ) -> Result<&KeySharePublic> {
+        self.keygen_output
+            .public_key_shares()
+            .iter()
+            .find(|item| item.participant() == pid)
+            .ok_or_else(|| {
+                error!("Presign input doesn't contain a public keyshare for {}, even though we checked for it at construction.", pid);
+                InternalError::InternalInvariantFailed
+            })
+    }
+
+    /// Returns the [`AuxInfoPublic`]s associated with all the participants
+    /// _except_ the given [`ParticipantIdentifier`].
+    pub(crate) fn all_but_one_auxinfo_public(
+        &self,
+        pid: ParticipantIdentifier,
+    ) -> Vec<&AuxInfoPublic> {
+        self.auxinfo_output
+            .public_auxinfo()
+            .iter()
+            .filter(|item| item.participant() != pid)
+            .collect()
+    }
+    /// Returns a copy of the [`AuxInfoPublic`]s associated with all the
+    /// participants (including this participant).
+    pub(crate) fn to_public_auxinfo(&self) -> Vec<AuxInfoPublic> {
+        self.auxinfo_output.public_auxinfo().to_vec()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Input;
+    use crate::{
+        auxinfo,
+        errors::{CallerError, InternalError, Result},
+        keygen,
+        keyrefresh::KeyrefreshParticipant,
+        utils::testing::init_testing,
+        Identifier, ParticipantConfig, ParticipantIdentifier, ProtocolParticipant,
+    };
+
+    #[test]
+    fn inputs_must_be_same_length() {
+        let rng = &mut init_testing();
+
+        let pids = std::iter::repeat_with(|| ParticipantIdentifier::random(rng))
+            .take(5)
+            .collect::<Vec<_>>();
+        let keygen_output = keygen::Output::simulate(&pids, rng);
+        let auxinfo_output = auxinfo::Output::simulate(&pids, rng);
+
+        // Same length works
+        let result = Input::new(auxinfo_output.clone(), keygen_output.clone());
+        assert!(result.is_ok());
+
+        // If keygen is too short, it fails.
+        let short_keygen = keygen::Output::simulate(&pids[1..], rng);
+        let result = Input::new(auxinfo_output, short_keygen);
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            InternalError::CallingApplicationMistake(CallerError::BadInput)
+        );
+
+        // If auxinfo is too short, it fails.
+        let short_auxinfo = auxinfo::Output::simulate(&pids[1..], rng);
+        let result = Input::new(short_auxinfo, keygen_output);
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            InternalError::CallingApplicationMistake(CallerError::BadInput)
+        );
+    }
+
+    #[test]
+    fn inputs_must_have_same_participant_sets() {
+        let rng = &mut init_testing();
+
+        let auxinfo_pids = std::iter::repeat_with(|| ParticipantIdentifier::random(rng))
+            .take(5)
+            .collect::<Vec<_>>();
+        let auxinfo_output = auxinfo::Output::simulate(&auxinfo_pids, rng);
+
+        let keygen_pids = std::iter::repeat_with(|| ParticipantIdentifier::random(rng))
+            .take(5)
+            .collect::<Vec<_>>();
+        let keygen_output = keygen::Output::simulate(&keygen_pids, rng);
+
+        let result = Input::new(auxinfo_output, keygen_output);
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            InternalError::CallingApplicationMistake(CallerError::BadInput)
+        );
+    }
+
+    #[test]
+    fn protocol_participants_must_match_input_participants() -> Result<()> {
+        let rng = &mut init_testing();
+        let SIZE = 5;
+
+        // Create valid input set with random PIDs
+        let input_pids = std::iter::repeat_with(|| ParticipantIdentifier::random(rng))
+            .take(SIZE)
+            .collect::<Vec<_>>();
+        let keygen_output = keygen::Output::simulate(&input_pids, rng);
+        let auxinfo_output = auxinfo::Output::simulate(&input_pids, rng);
+
+        let input = Input::new(auxinfo_output, keygen_output)?;
+
+        // Create valid config with PIDs independent of those used to make the input set
+        let config = ParticipantConfig::random(SIZE, rng);
+
+        let result = KeyrefreshParticipant::new(
+            Identifier::random(rng),
+            config.id(),
+            config.other_ids().to_vec(),
+            input,
+        );
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            InternalError::CallingApplicationMistake(CallerError::BadInput)
+        );
+
+        Ok(())
+    }
+}
---
+++ src/keyrefresh/keyshare.rs
@@ -0,0 +1,349 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// Modifications Copyright (c) 2022-2023 Bolt Labs Holdings, Inc
+//
+// This source code is licensed under both the MIT license found in the
+// LICENSE-MIT file in the root directory of this source tree and the Apache
+// License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+// of this source tree.
+
+use crate::{
+    errors::{CallerError, InternalError, Result},
+    paillier::{Ciphertext, DecryptionKey, EncryptionKey},
+    utils::{k256_order, CurvePoint, ParseBytes},
+    ParticipantIdentifier,
+};
+use libpaillier::unknown_order::BigNumber;
+use rand::{CryptoRng, RngCore};
+use serde::{Deserialize, Serialize};
+use std::fmt::Debug;
+use tracing::error;
+use zeroize::{Zeroize, ZeroizeOnDrop};
+
+const KEYSHARE_TAG: &[u8] = b"KeySharePrivate";
+
+/// Encrypted [`KeyUpdatePrivate`].
+#[derive(Clone, Serialize, Deserialize)]
+pub struct KeyUpdateEncrypted {
+    ciphertext: Ciphertext,
+}
+
+impl KeyUpdateEncrypted {
+    pub fn encrypt<R: RngCore + CryptoRng>(
+        update: &KeyUpdatePrivate,
+        pk: &EncryptionKey,
+        rng: &mut R,
+    ) -> Result<Self> {
+        // TODO: Add this requirement in AuxInfo.
+        if &(k256_order() * 2) >= pk.modulus() {
+            error!("KeyUpdateEncrypted encryption failed, pk.modulus() is too small");
+            Err(InternalError::InternalInvariantFailed)?;
+        }
+
+        let (ciphertext, _nonce) = pk
+            .encrypt(rng, &update.x)
+            .map_err(|_| InternalError::InternalInvariantFailed)?;
+
+        Ok(KeyUpdateEncrypted { ciphertext })
+    }
+
+    pub fn decrypt(&self, dk: &DecryptionKey) -> Result<KeyUpdatePrivate> {
+        let x = dk.decrypt(&self.ciphertext).map_err(|_| {
+            error!("KeyUpdateEncrypted decryption failed, ciphertext out of range",);
+            CallerError::DeserializationFailed
+        })?;
+        if x >= k256_order() || x < BigNumber::one() {
+            Err(CallerError::DeserializationFailed)?;
+        }
+        Ok(KeyUpdatePrivate { x })
+    }
+}
+
+/// Private update corresponding to a given [`Participant`](crate::Participant)'s.
+///
+/// # 🔒 Storage requirements
+/// This type must be stored securely by the calling application.
+#[derive(Clone, ZeroizeOnDrop, PartialEq, Eq, Serialize, Deserialize)]
+pub struct KeyUpdatePrivate {
+    x: BigNumber, // in the range [1, q)
+}
+
+impl Debug for KeyUpdatePrivate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("KeySharePrivate([redacted])")
+    }
+}
+
+impl KeyUpdatePrivate {
+    /// Sample a private key share uniformly at random.
+    pub(crate) fn random(rng: &mut (impl CryptoRng + RngCore)) -> Self {
+        let random_bn = BigNumber::from_rng(&k256_order(), rng);
+        KeyUpdatePrivate { x: random_bn }
+    }
+
+    /// Compute a private key share such that the sum of all shares equals 0 mod q.
+    pub(crate) fn zero_sum(others: &[Self]) -> Self {
+        let sum = others
+            .iter()
+            .fold(BigNumber::zero(), |sum, o| sum + o.x.clone());
+        let balance = (-sum).nmod(&k256_order());
+        KeyUpdatePrivate { x: balance }
+    }
+
+    pub(crate) fn sum(shares: &[Self]) -> Self {
+        let sum = shares
+            .iter()
+            .fold(BigNumber::zero(), |sum, o| sum + o.x.clone())
+            .nmod(&k256_order());
+        KeyUpdatePrivate { x: sum }
+    }
+
+    /// Computes the "raw" curve point corresponding to this private key.
+    pub(crate) fn public_share(&self) -> Result<CurvePoint> {
+        CurvePoint::GENERATOR.multiply_by_bignum(&self.x)
+    }
+
+    /// Convert private material into bytes.
+    ///
+    /// 🔒 This is intended for use by the calling application for secure
+    /// storage. The output of this function should be handled with care.
+    pub fn into_bytes(self) -> Vec<u8> {
+        // Format:
+        // KEYSHARE_TAG | key_len in bytes | key (big endian bytes)
+        //              | 8 bytes          | key_len bytes
+
+        let mut share = self.x.to_bytes();
+        let share_len = share.len().to_le_bytes();
+
+        let bytes = [KEYSHARE_TAG, &share_len, &share].concat();
+        share.zeroize();
+        bytes
+    }
+
+    /// Convert bytes into private material.
+    ///
+    /// 🔒 This is intended for use by the calling application for secure
+    /// storage. Do not use this method to create arbitrary instances of
+    /// [`KeySharePrivate`].
+    pub fn try_from_bytes(bytes: Vec<u8>) -> Result<Self> {
+        // Expected format:
+        // KEYSHARE_TAG | key_len in bytes | key (big endian bytes)
+        //              | 8 bytes          | key_len bytes
+
+        let mut parser = ParseBytes::new(bytes);
+
+        // This little function ensures that
+        // 1. We can zeroize out the potentially-sensitive input bytes regardless of
+        //    whether parsing succeeded; and
+        // 2. We can log the error message once at the end, rather than duplicating it
+        //    across all the parsing code
+        let mut parse = || {
+            // Make sure the KEYSHARE_TAG is correct.
+            let actual_tag = parser.take_bytes(KEYSHARE_TAG.len())?;
+            if actual_tag != KEYSHARE_TAG {
+                Err(CallerError::DeserializationFailed)?
+            }
+
+            // Extract the length of the key share
+            let share_len = parser.take_len()?;
+
+            let share_bytes = parser.take_rest()?;
+            if share_bytes.len() != share_len {
+                Err(CallerError::DeserializationFailed)?
+            }
+
+            // Check that the share itself is valid
+            let share = BigNumber::from_slice(share_bytes);
+            if share > k256_order() || share < BigNumber::one() {
+                Err(CallerError::DeserializationFailed)?
+            }
+
+            Ok(Self { x: share })
+        };
+
+        let result = parse();
+
+        // During parsing, we copy all the bytes we need into the appropriate types.
+        // Here, we delete the original copy.
+        parser.zeroize();
+
+        // Log a message in case of error
+        if result.is_err() {
+            error!(
+                "Failed to deserialize `KeySharePrivate. Expected format:
+                        {:?} | share_len | share
+                        where `share_len` is a little-endian encoded usize
+                        and `share` is exactly `share_len` bytes long.",
+                KEYSHARE_TAG
+            );
+        }
+        result
+    }
+}
+
+impl AsRef<BigNumber> for KeyUpdatePrivate {
+    /// Get the private key share.
+    fn as_ref(&self) -> &BigNumber {
+        &self.x
+    }
+}
+
+/// A curve point representing a given [`Participant`](crate::Participant)'s
+/// public key.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct KeyUpdatePublic {
+    participant: ParticipantIdentifier,
+    X: CurvePoint,
+}
+
+impl KeyUpdatePublic {
+    pub(crate) fn new(participant: ParticipantIdentifier, share: CurvePoint) -> Self {
+        Self {
+            participant,
+            X: share,
+        }
+    }
+
+    /// Get the ID of the participant who claims to hold the private share
+    /// corresponding to this public key share.
+    pub fn participant(&self) -> ParticipantIdentifier {
+        self.participant
+    }
+
+    /// Generate a new [`KeySharePrivate`] and [`KeySharePublic`].
+    pub(crate) fn new_keyshare<R: RngCore + CryptoRng>(
+        participant: ParticipantIdentifier,
+        rng: &mut R,
+    ) -> Result<(KeyUpdatePrivate, KeyUpdatePublic)> {
+        let private_share = KeyUpdatePrivate::random(rng);
+        let public_share = private_share.public_share()?;
+
+        Ok((
+            private_share,
+            KeyUpdatePublic::new(participant, public_share),
+        ))
+    }
+
+    pub(crate) fn sum(participant: ParticipantIdentifier, shares: &[Self]) -> Self {
+        let sum = shares.iter().fold(CurvePoint::IDENTITY, |sum, o| sum + o.X);
+        Self {
+            participant,
+            X: sum,
+        }
+    }
+}
+
+impl AsRef<CurvePoint> for KeyUpdatePublic {
+    /// Get the public curvepoint which is the public key share.
+    fn as_ref(&self) -> &CurvePoint {
+        &self.X
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        keyrefresh::{keyshare::KEYSHARE_TAG, KeyUpdatePrivate},
+        utils::{k256_order, testing::init_testing},
+    };
+
+    #[test]
+    fn keyshare_private_bytes_conversion_works() {
+        let rng = &mut init_testing();
+        let share = KeyUpdatePrivate::random(rng);
+
+        let bytes = share.clone().into_bytes();
+        let reconstructed = KeyUpdatePrivate::try_from_bytes(bytes);
+
+        assert!(reconstructed.is_ok());
+        assert_eq!(reconstructed.unwrap(), share);
+    }
+
+    #[test]
+    fn keyshare_private_bytes_must_be_in_range() {
+        // Share must be < k256_order()
+        let too_big = KeyUpdatePrivate {
+            x: k256_order() + 1,
+        };
+        let bytes = too_big.into_bytes();
+        assert!(KeyUpdatePrivate::try_from_bytes(bytes).is_err());
+
+        // Note: I tried testing the negative case but it seems like the
+        // unknown_order crate's `from_bytes` method always interprets
+        // numbers as positive. Unfortunately the crate does not
+        // document the expected representation only noting that it
+        // takes a big-endian byte sequence.
+    }
+
+    #[test]
+    fn deserialized_keyshare_private_tag_must_be_correct() {
+        let rng = &mut init_testing();
+        let key_share = KeyUpdatePrivate::random(rng);
+
+        // Cut out the tag from the serialized bytes for convenience.
+        let share_bytes = &key_share.into_bytes()[KEYSHARE_TAG.len()..];
+
+        // Tag must have correct content
+        let wrong_tag = b"NotTheRightTag!";
+        assert_eq!(wrong_tag.len(), KEYSHARE_TAG.len());
+        let bad_bytes = [wrong_tag.as_slice(), share_bytes].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bad_bytes).is_err());
+
+        // Tag must be correct length (too short, too long)
+        let short_tag = &KEYSHARE_TAG[..5];
+        let bad_bytes = [short_tag, share_bytes].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bad_bytes).is_err());
+
+        let bad_bytes = [KEYSHARE_TAG, b"TAG EXTENSION!", share_bytes].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bad_bytes).is_err());
+
+        // Normal serialization works
+        let bytes = [KEYSHARE_TAG, share_bytes].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bytes).is_ok());
+    }
+
+    #[test]
+    fn deserialized_keyshare_private_length_field_must_be_correct() {
+        let rng = &mut init_testing();
+        let share_bytes = KeyUpdatePrivate::random(rng).x.to_bytes();
+
+        // Length must be specified
+        let bad_bytes = [KEYSHARE_TAG, &share_bytes].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bad_bytes).is_err());
+
+        // Length must be little endian
+        let share_len = share_bytes.len().to_be_bytes();
+        let bad_bytes = [KEYSHARE_TAG, &share_len, &share_bytes].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bad_bytes).is_err());
+
+        // Length must be correct (too long, too short)
+        let too_short = (share_bytes.len() - 5).to_le_bytes();
+        let bad_bytes = [KEYSHARE_TAG, &too_short, &share_bytes].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bad_bytes).is_err());
+
+        let too_long = (share_bytes.len() + 5).to_le_bytes();
+        let bad_bytes = [KEYSHARE_TAG, &too_long, &share_bytes].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bad_bytes).is_err());
+
+        // Correct length works
+        let share_len = share_bytes.len().to_le_bytes();
+        let bytes = [KEYSHARE_TAG, &share_len, &share_bytes].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bytes).is_ok());
+    }
+
+    #[test]
+    fn deserialized_keyshare_private_requires_all_fields() {
+        // Part of a tag or the whole tag alone doesn't pass
+        let bytes = &KEYSHARE_TAG[..3];
+        assert!(KeyUpdatePrivate::try_from_bytes(bytes.to_vec()).is_err());
+        assert!(KeyUpdatePrivate::try_from_bytes(KEYSHARE_TAG.to_vec()).is_err());
+
+        // Length with no secret following doesn't pass
+        let share_len = k256_order().bit_length() / 8;
+        let bytes = [KEYSHARE_TAG, &share_len.to_le_bytes()].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bytes).is_err());
+
+        // Zero-length doesn't pass
+        let bytes = [KEYSHARE_TAG, &0usize.to_le_bytes()].concat();
+        assert!(KeyUpdatePrivate::try_from_bytes(bytes).is_err());
+    }
+}
